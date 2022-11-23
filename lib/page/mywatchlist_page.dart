76720a680ab2e164d1a9496296b9a440ac9022b8
amanda.nurul11 <amanda.nurul11@ui.ac.id>
2022-11-23 16:39:04 +0700

    Finish bonus tasks for Tugas 9

--- lib/page/mywatchlist_page.dart
+++ lib/page/mywatchlist_page.dart
@@ -49,8 +49,11 @@ class _MyWatchListPageState extends State<MyWatchListPage> {
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: const [
-                      BoxShadow(color: Colors.black, blurRadius: 2.0)
+                    boxShadow:  [
+                      BoxShadow(
+                        color:snapshot.data![index].fields.watched ? Colors.blue : Colors.red, 
+                        blurRadius: 2.0
+                      )
                     ]
                   ),
                   child: Column(
@@ -92,10 +95,9 @@ class _MyWatchListPageState extends State<MyWatchListPage> {
                             ),
                           );
                         } 
-                      ),  
+                      ), 
                     ], 
                   ),
-                  
                 ),
               );
             }
